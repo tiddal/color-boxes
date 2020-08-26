6b393a115d932e5a2678beb81e2e9efdcfe33728
tiddal <tiddal96@gmail.com>
2020-08-26 17:59:59 +0100

    style: boxes position

--- src/components/Box/styles.ts
+++ src/components/Box/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   background-color: ${({ color }) => color};
-  height: 10rem;
   cursor: pointer;
 `;
 
--- src/pages/ColorBoxes/styles.ts
+++ src/pages/ColorBoxes/styles.ts
@@ -4,12 +4,16 @@ const Container = styled.div`
   padding: 2rem;
   display: grid;
   grid-template-columns: repeat(5, 1fr);
-  grid-template-rows: repeat(5, 1fr);
+  grid-template-rows: auto repeat(4, 1fr);
+  align-content: center;
+  height: 100vh;
+
   span {
     grid-column: -1 / 1;
     justify-self: center;
-    align-self: center;
+    align-self: start;
     text-align: center;
+    padding-bottom: 2rem;
     h1 {
       text-transform: uppercase;
       color: #333;
